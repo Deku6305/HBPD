a284f49fd4c280f4b237d2bedaf6703748ba8739
Deku6305 <tuandat6305@gmail.com>
2025-07-30 01:38:11 +0700

    2

--- happy birtday.js
+++ happy birtday.js
@@ -1,14 +1,14 @@
+let c = document.getElementById("c");
 let w = (c.width = window.innerWidth),
   h = (c.height = window.innerHeight),
   ctx = c.getContext("2d"),
-  hw = w / 2;
-(hh = h / 2),
-  (opts = {
-    // change the text in here //
+  hw = w / 2,
+  hh = h / 2,
+  opts = {
     strings: ["HAPPY", "BIRTHDAY!", "NGUYỄN THỊ NHƯ HUỲNH❤️"],
-    charSize: 30,
-    charSpacing: 35,
-    lineHeight: 40,
+    charSize: 20,
+    charSpacing: 25,
+    lineHeight: 30,
 
     cx: w / 2,
     cy: h / 2,
@@ -19,8 +19,8 @@ let w = (c.width = window.innerWidth),
     fireworkSpawnTime: 200,
     fireworkBaseReachTime: 30,
     fireworkAddedReachTime: 30,
-    fireworkCircleBaseSize: 20,
-    fireworkCircleAddedSize: 10,
+    fireworkCircleBaseSize: 15,
+    fireworkCircleAddedSize: 8,
     fireworkCircleBaseTime: 30,
     fireworkCircleAddedTime: 30,
     fireworkCircleFadeBaseTime: 10,
@@ -30,29 +30,29 @@ let w = (c.width = window.innerWidth),
     fireworkShardPrevPoints: 3,
     fireworkShardBaseVel: 4,
     fireworkShardAddedVel: 2,
-    fireworkShardBaseSize: 3,
-    fireworkShardAddedSize: 3,
+    fireworkShardBaseSize: 2,
+    fireworkShardAddedSize: 2,
     gravity: 0.1,
     upFlow: -0.1,
     letterContemplatingWaitTime: 360,
     balloonSpawnTime: 20,
     balloonBaseInflateTime: 10,
     balloonAddedInflateTime: 10,
-    balloonBaseSize: 20,
-    balloonAddedSize: 20,
+    balloonBaseSize: 15,
+    balloonAddedSize: 15,
     balloonBaseVel: 0.4,
     balloonAddedVel: 0.4,
     balloonBaseRadian: -(Math.PI / 2 - 0.5),
     balloonAddedRadian: -1,
-  }),
-  (calc = {
+  },
+  calc = {
     totalWidth:
       opts.charSpacing *
       Math.max(opts.strings[0].length, opts.strings[1].length),
-  }),
-  (Tau = Math.PI * 2),
-  (TauQuarter = Tau / 4),
-  (letters = []);
+  },
+  Tau = Math.PI * 2,
+  TauQuarter = Tau / 4,
+  letters = [];
 
 ctx.font = opts.charSize + "px Verdana";
 
@@ -393,12 +393,14 @@ anim();
 window.addEventListener("resize", function () {
   w = c.width = window.innerWidth;
   h = c.height = window.innerHeight;
-
   hw = w / 2;
   hh = h / 2;
-
+  opts.cx = w / 2;
+  opts.cy = h / 2;
   ctx.font = opts.charSize + "px Verdana";
+  letters.forEach((letter) => letter.reset());
 });
+
 function hienThu() {
   document.getElementById("thu-chuc").style.display = "block";
 }
